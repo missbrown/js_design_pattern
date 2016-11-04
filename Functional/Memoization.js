251e052988e04fa2f41b90499fe9cbc544a1de8d
杨晓磊 <xiaolei.yang@china.zhaogang.com>
2016-11-04 16:02:29 +0800

    'commit'

--- Functional/Memoization.js
+++ Functional/Memoization.js
@@ -1,3 +1,31 @@
 /**
  * Created by xiaolei.yang on 2016/11/4.
  */
+function naiveFibonacci(n) {
+    if(isNaN(n) || n < 0)
+        throw 'Illegal Argument';
+    if(n < 2) return 1;
+    else {
+        return naiveFibonacci(n-1) + naiveFibonacci(n-2);
+    }
+}
+
+var memoizationFibonacci = (function(){
+    var fibo = [];
+    fibo[0] = 1;
+    fibo[1] = 1;
+    return function(n) {
+        if(isNaN(n) || n < 0)
+            throw 'Illegal Argument';
+        if(n < 2) return 1;
+        else {
+            if(!fibo[n]) {
+                fibo[n] = memoizationFibonacci(n-1) + memoizationFibonacci(n-2);
+            }
+            return fibo[n];
+        }
+    }
+})();
+
+//console.log(naiveFibonacci(40));
+console.log(memoizationFibonacci(40));
--- test_pattern/Spy.js
+++ test_pattern/Spy.js
@@ -1,3 +1,27 @@
 /**
  * Created by xiaolei.yang on 2016/11/4.
  */
+var SpyUpon = (function() {
+    function SpyUpon() {}
+    SpyUpon.prototype.write = function(toWrite) {
+        console.log(toWrite);
+        return 7;
+    };
+    return SpyUpon;
+})();
+
+var spyUpon = new SpyUpon();
+spyUpon._write = spyUpon.write;
+spyUpon.write = function(arg1) {
+    console.log("intercepted");
+    this.called = true;
+    this.args = arguments;
+    this.result = this._write(arg1);
+    return this.result;
+};
+
+// it is convenient to use spy even the 'write' function is a third party function
+console.log(spyUpon.write("hello world"));
+console.log(spyUpon.called);
+console.log(spyUpon.args);
+console.log(spyUpon.result);
